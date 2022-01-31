235a8f8066f1988420374be5eb3245c108adf5b5
Moshe Beladev <moshebe123@gmail.com>
2022-01-31 09:54:56 +0200

    embed version info

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/moshebe/gtrace/internal/cli"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+)
+
+func versionName() string {
+	if !strings.Contains(version, "-") {
+		return version
+	}
+	return fmt.Sprintf("%s (%s)", version, commit)
+}
+
 func main() {
-	err := cli.App.Run(os.Args)
+	err := cli.App(versionName()).Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
